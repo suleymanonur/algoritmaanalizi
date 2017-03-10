23af67a92a4494dd523359f3a61e5d39181a7145
suleymanonur <suleymanmiri123@gmail.com>
2017-03-10 13:06:19 +0200

    Update diziyiİkiyeBolme.cpp

--- diziyiİkiyeBolme.cpp
+++ diziyiİkiyeBolme.cpp
@@ -1,31 +1,34 @@
 #include <cstdlib>
 #include <iostream>
-
-
-
+#include <vector>
 
 using namespace std;
 
-void yazdir(int dizi[], int elemanSayisi){
-     int i;
-     for(i=elemanSayisi/2;i>0; i--){
-         cout << dizi[i-1] << " ";        
-    
-    }  
-    cout << endl;
-    
-    for(i=elemanSayisi/2;i<elemanSayisi; i++){
-         cout << dizi[i] << " ";         
-                  
-    }         
-
-     
+void yazdir(int dizi[],int l)
+{
+     int t1=0,t2=0;
+     int tut1=dizi[l/2-1], tut2=dizi[l/2];
+     int i,j;
+     for(i=l/2-1;i>=0;i--)
+     {
+         t1=t1+dizi[i];
+         if(tut1<t1) tut1=t1;
+         cout<<dizi[i]<<",";
+     }
+     cout<<endl;
+     for(i=l/2;i<l;i++)
+     {
+         t2=t2+dizi[i];
+         if(tut2<t2) tut2=t2;
+         cout<<dizi[i]<<",";
+     }
+     cout<<endl<<tut1<<endl<<tut2<<endl;
 }
+
 int main(int argc, char *argv[])
 {
-    int dizi[] = {4,-3,2,1,6,-1,-2,4};
+    int dizi[]={4,-3,2,1,6,-1,-2,4};
     yazdir(dizi,8);
-         
     system("PAUSE");
     return EXIT_SUCCESS;
 }
